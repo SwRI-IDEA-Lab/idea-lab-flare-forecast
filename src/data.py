26fb04c1d2beef7a244d1271fd063aa6b8f88581
kierav <kiera.vds@gmail.com>
2023-02-23 08:06:47 -0700

    Add data normalization

--- src/data.py
+++ src/data.py
@@ -12,6 +12,7 @@ class MagnetogramDataSet(torch.utils.data.Dataset):
     def __init__(self,df):
         self.name_frame = df.loc[:,'filename']
         self.label_frame = df.loc[:,'flare']
+        self.dataset_frame = df.loc[:,'dataset']
 
     def __len__(self):
         return len(self.name_frame)
@@ -30,6 +31,17 @@ class MagnetogramDataSet(torch.utils.data.Dataset):
         img = np.array(h5py.File(filename,'r')['magnetogram']).astype(np.float32)
         img = np.nan_to_num(img)
         label = self.label_frame.iloc[idx]
+
+        # Normalize magnetogram data
+        # 1.3 calibration factor for MDI data
+        if self.dataset_frame.iloc[idx] == 'mdi':
+            img = img/1.3
+        # clip magnetogram data within max value
+        maxval = 2000  # Gauss
+        img[np.where(img>maxval)] = maxval
+        img[np.where(img<-maxval)] = -maxval
+        # scale between -1 and 1
+        img = img/maxval
 
         return [filename,img,label]
     
--- src/data_test.py
+++ src/data_test.py
@@ -11,6 +11,7 @@ class DataTest(unittest.TestCase):
         self.dataset = MagnetogramDataSet(self.df)
         self.labels = [0,1]
         self.dim = 256
+        self.idx = 0        # index into dataset (must be within length of self.dataset)
 
     def test_datasetExists(self):
         self.assertGreaterEqual(len(self.dataset),0)
@@ -19,15 +20,23 @@ class DataTest(unittest.TestCase):
         self.assertIsNotNone(self.dataset[0])
 
     def test_itemType(self):
-        item = self.dataset[0]
+        item = self.dataset[self.idx]
         self.assertIsInstance(item,list)        
         self.assertEqual(len(item),3)
+
+    def test_itemContents(self):
+        item = self.dataset[self.idx]
         self.assertIsInstance(item[0],str)
         self.assertIsInstance(item[1],np.ndarray)
         self.assertTrue(item[1].dtype==np.dtype('float32'))
-        self.assertTrue(not np.isnan(item[1]).any())
         self.assertTrue(np.shape(item[1])==(self.dim,self.dim))
         self.assertTrue(item[2] in self.labels)
+    
+    def test_dataNormalization(self):
+        for idx in range(len(self.dataset)):
+            item = self.dataset[idx]
+            data = item[1]
+            self.assertLessEqual(np.max(np.abs(data)),1)
 
 
 if __name__ == "__main__":
