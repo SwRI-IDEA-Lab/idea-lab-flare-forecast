408dab8e8cc1eaf185fcc5d1f0878c9d3662a380
kierav <kiera.vds@gmail.com>
2023-03-08 14:13:18 -0700

    working parameters

--- src/train_model.py
+++ src/train_model.py
@@ -17,8 +17,8 @@ def main():
     datafile = 'Data/labels_MDI.csv'
     window = 24     # forecast window (hours)
     dim = 128
-    lr = 1e-3
-    wd = 1e-2
+    lr = 1e-4
+    wd = 1e-3
     dropout_ratio = 0
     batch = 32
 
@@ -53,18 +53,6 @@ def main():
     trainer.fit(model=classifier,datamodule=data)
 
     # evaluate model
-    print('Evaluation on training set')
-    predictions = trainer.predict(classifier,data.train_dataloader())
-    y_pred = predictions[0][0]
-    y_true = predictions[0][1]
-    print('Predicted: ',torch.transpose(y_pred,0,1))
-    print('True:', torch.transpose(y_true,0,1))
-    print('Evaluation on validation set')
-    predictions = trainer.predict(classifier,data.val_dataloader())
-    y_pred = predictions[0][0]
-    y_true = predictions[0][1]
-    print('Predicted: ',torch.transpose(y_pred,0,1))
-    print('True:', torch.transpose(y_true,0,1))
 
 if __name__ == "__main__":
     main()
