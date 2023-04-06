b8c5973c3a8c48c376efe980b5897436a313ed3c
kierav <kiera.vds@gmail.com>
2023-04-06 15:41:52 -0600

    loop through all run ids

--- src/eval_models.py
+++ src/eval_models.py
@@ -3,9 +3,9 @@ import yaml
 import eval_model
 
 # Run IDs for pretraining experiment
-run_ids = ['0yp1u3u7','rqjod26l','ccibb0m3','l43yawpd','kb7e4vjm']  # trained from scratch
-# run_ids = ['yhhzi3ol','ckit3ek0','i1riak8a','ju5soz52','cx3n24vr']  # pre-trained on high flux
-# run_ids = ['rmvre2nt','myhjb5c6','jkk8ysjw','gud9szjr','nnchky6w']  # pre-trained on flare-flux
+run_ids = ['0yp1u3u7','rqjod26l','ccibb0m3','l43yawpd','kb7e4vjm',  # trained from scratch
+        'yhhzi3ol','ckit3ek0','i1riak8a','ju5soz52','cx3n24vr',  # pre-trained on high flux
+        'rmvre2nt','myhjb5c6','jkk8ysjw','gud9szjr','nnchky6w']  # pre-trained on flare-flux
 
 with open('experiment_config.yml') as config_file:
     config = yaml.safe_load(config_file.read())
