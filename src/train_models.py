7ebe47c2f9499cc117d79a77658191919bacf760
kierav <kiera.vds@gmail.com>
2023-06-06 14:35:21 -0600

    add pretrain step to train models script

--- src/train_models.py
+++ src/train_models.py
@@ -13,25 +13,25 @@ for i in range(5):
     config['data']['val_split'] = i
 
     # first pretrain
-    # config['data']['label'] = 'high_flux'
-    # config['data']['augmentation'] = 'conservative'
-    # config['model']['checkpoint_location'] = None
-    # config['model']['load_checkpoint'] = False
-    # config['testing']['eval'] = False
-    # with open('experiment_config.yml','w') as config_file:
-    #     yaml.dump(config,config_file)
+    config['data']['label'] = 'high_flux'
+    config['data']['augmentation'] = 'conservative'
+    config['model']['checkpoint_location'] = None
+    config['model']['load_checkpoint'] = False
+    config['testing']['eval'] = False
+    with open('experiment_config.yml','w') as config_file:
+        yaml.dump(config,config_file)
     
-    # train_model.main()
+    train_model.main()
 
     # # obtain run id and run train
-    # last_run = sorted(os.listdir('wandb'))[-1]
-    # run_id = last_run.split('-')[-1]
-    run_id = highfluxmodels[i]
+    last_run = sorted(os.listdir('wandb'))[-1]
+    run_id = last_run.split('-')[-1]
+    # run_id = highfluxmodels[i]
     config['data']['label'] = 'flare'
     config['data']['augmentation'] = 'conservative'
     config['model']['checkpoint_location'] = 'kierav/flare-forecast/model-'+run_id+':best_k'
     config['model']['load_checkpoint'] = True
-    config['testing']['eval'] = False
+    config['testing']['eval'] = True
     with open('experiment_config.yml','w') as config_file:
         yaml.dump(config,config_file)
 
