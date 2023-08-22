c169b75cebcd31bf584f0228e33b3e03b7d1428e
kierav <kiera.vds@gmail.com>
2023-08-22 13:24:06 -0600

    set up hmi zarr experiment

--- src/train_model_regression.py
+++ src/train_model_regression.py
@@ -7,6 +7,7 @@ from pytorch_lightning.callbacks import ModelSummary, ModelCheckpoint
 from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from model_regressor import convnet_sc_regressor,LitConvNetRegressor
 from data import MagnetogramDataModule
+from data_zarr import AIAHMIDataModule
 from mlp_model import MLPModel
 from utils.model_utils import *
 import pandas as pd
@@ -34,21 +35,34 @@ def main():
     print('Features:',config.data['feature_cols'])
 
     # define data module
-    data = MagnetogramDataModule(data_file=config.data['data_file'],
-                                 label=config.data['label'],
-                                 balance_ratio=config.data['balance_ratio'],
-                                 regression=config.data['regression'],
-                                 val_split=config.data['val_split'],
-                                 forecast_window=config.data['forecast_window'],
-                                 dim=config.data['dim'],
-                                 batch=config.training['batch_size'],
-                                 augmentation=config.data['augmentation'],
-                                 flare_thresh=config.data['flare_thresh'],
-                                 flux_thresh=config.data['flux_thresh'],
-                                 feature_cols=config.data['feature_cols'],
-                                 test=config.data['test'],
-                                 maxval=config.data['maxval'],
-                                 file_col=config.data['file_col'])
+    # data = MagnetogramDataModule(data_file=config.data['data_file'],
+    #                              label=config.data['label'],
+    #                              balance_ratio=config.data['balance_ratio'],
+    #                              regression=config.data['regression'],
+    #                              val_split=config.data['val_split'],
+    #                              forecast_window=config.data['forecast_window'],
+    #                              dim=config.data['dim'],
+    #                              batch=config.training['batch_size'],
+    #                              augmentation=config.data['augmentation'],
+    #                              flare_thresh=config.data['flare_thresh'],
+    #                              flux_thresh=config.data['flux_thresh'],
+    #                              feature_cols=config.data['feature_cols'],
+    #                              test=config.data['test'],
+    #                              maxval=config.data['maxval'],
+    #                              file_col=config.data['file_col'])
+    
+    data = AIAHMIDataModule(zarr_file=config.data['zarr_file'],
+                            data_file=config.data['data_file'],
+                            regression=config.data['regression'],
+                            forecast_window=config.data['forecast_window'],
+                            dim=config.data['dim'],
+                            batch=config.training['batch_size'],
+                            augmentation=config.data['augmentation'],
+                            flare_thresh=config.data['flare_thresh'],
+                            feature_cols=config.data['feature_cols'],
+                            test=config.data['test'],
+                            channels=config.data['channels'],
+                            maxval=config.data['maxval'],)
 
     # train MLP model to obtain weights for final layers of CNN+MLP
     if len(config.data['feature_cols'])>0:
